139aa96f5478a91c2a7bc4f34d370aca1fc5b0be
KaiqueMunhoz <munhoz13km@gmail.com>
2018-05-03 22:28:02 -0300

    Added a style to the game

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+//CSS
+import './index.css';
+
 //Components
 import Game from './components/Game';
 
